0437c8ecfad279a324d903dfb7e1ce5982b00be9
Partha Acharjee <partha.rlc@gmail.com>
2017-07-26 20:39:41 -0500

    Regularization demo updated.

--- RegularizationDemo.py
+++ RegularizationDemo.py
@@ -5,56 +5,29 @@ from sklearn import linear_model
 # create dataset for a semi-circle
 N=1000
 x=np.linspace(-1,1,N)
-y=x**2+np.sqrt(1-x**2)+np.random.normal(0,0.1,N)
+y=np.sqrt(1-x**2)+np.random.normal(0,0.1,N)
 
 plt.close('all')
 
-#### Calculate error and fiting using 1-9th polynomial
-#f, ax = plt.subplots(3,3, sharex=True, sharey=True)
-#for i in range(1,10):
-#    z = np.polyfit(x, y, i)
-#    
-#    ithPoly=np.poly1d(z)
-#    yest=ithPoly(x)
-#    error=np.mean((yest-y)**2)
-#
-#    
-#    plotid=(int(np.floor((i-1)/3)),(i-1)%3)
-#    ax[plotid].scatter(x,y,marker='.',alpha=0.2)
-#    ax[plotid].plot(x,yest, color='m')
-#    ax[plotid].set_title('Fiting model. %d,'%i+'Error: %0.4f'%error)
-#
-#plt.show()
-#
-#### Linear regression error and coefficient relation
-#M=50
-#error=np.zeros(M)
-#coef=np.zeros((M,M+1))
-#for i in range(1,M+1):
-#    z = np.polyfit(x, y, i)
-#    
-#    ithPoly=np.poly1d(z)
-#    yest=ithPoly(x)
-#    error[i-1]=np.mean((yest-y)**2)
-#    coef[i-1,0:i+1]=ithPoly
-#
-#f,ax=plt.subplots(1,2)
-#ax[0].imshow(abs(coef),interpolation='nearest'); ax[0].set_xlabel('Coeficient number'); ax[0].set_ylabel('Model number');
-#ax[1].plot(error,marker='o',ls='--');  ax[1].set_xlabel('Model number'); ax[1].set_ylabel('Error')
-#plt.show()
-
+### Linear regrassion fiting
 model=10
 X=np.zeros((N,model))
 for i in range(1,model):
     X[:,i-1]=pow(x,i)
 
-
-fig,subplot=plt.subplots(2,1,sharex=True)
+modelLinear = linear_model.LinearRegression()
+modelLinear.fit(X,y)
+coefsLinear=modelLinear.coef_
+errorLinear=np.mean((y-modelLinear.predict(X))**2)
 
 ## L2 regularization error and coefficient relation
 
 n_alphas = 200
 alphas = np.logspace(-10, 1, n_alphas)
+
+#alphas = [1e-100,1e-10,1e-9,1e-8,1e-7,1e-6,1e-5,1e-4,1e-3,1e-2,1e-1,1,10]
+#n_alphas=len(alphas)
+
 modelL2 = linear_model.Ridge(fit_intercept=False)
 
 
@@ -66,13 +39,6 @@ for a,k in zip(alphas,range(0,n_alphas)):
     coefsL2[k,:]=modelL2.coef_
     errorL2[k]=np.mean((y-modelL2.predict(X))**2)
  
-ax =subplot[0]
-
-ax.plot(alphas, coefsL2)
-ax.set_xscale('log')
-ax.set_title('L2 coefficients as a function of the regularization')
-ax.set_ylabel('weights')
-
 
 ## L1 regularization and coefficeint relation
 
@@ -87,6 +53,16 @@ for a,k in zip(alphas,range(0,n_alphas)):
     errorL1[k]=np.mean((y-modelL1.predict(X))**2)
 
 
+## Plot coefficients weights for different alphas
+fig,subplot=plt.subplots(2,1,sharex=True)
+
+ax =subplot[0]
+
+ax.plot(alphas, coefsL2)
+ax.set_xscale('log')
+ax.set_title('L2 coefficients as a function of the regularization')
+ax.set_ylabel('weights')
+
 ax=subplot[1]
 
 ax.plot(alphas, coefsL1,'--')
@@ -94,11 +70,11 @@ ax.set_xscale('log')
 ax.set_xlabel('alpha')
 ax.set_ylabel('weights')
 
-
 ax.set_title('L1 coefficients as a function of the regularization')
 plt.axis('tight')
 plt.show()
 
+## Plot Regularization (alpha) vs Error
 plt.figure()
 plt.plot(alphas,errorL1,'r',alphas,errorL2,'b')
 plt.xscale('log')
@@ -107,9 +83,10 @@ plt.ylabel('Error')
 plt.title('Regularization (alpha) vs Error')
 plt.show()
 
+## Plot Regularization (alpha) vs Coefficients==0
 plt.figure()
-L1zeros=np.sum(coefsL1==0,axis=1)
-L2zeros=np.sum(coefsL2==0,axis=1)
+L1zeros=np.sum(abs(coefsL1)<1e-15,axis=1)
+L2zeros=np.sum(abs(coefsL2)<1e-15,axis=1)
 plt.plot(alphas,L1zeros,'r',alphas,L2zeros,'b')
 plt.xscale('log')
 plt.xlabel('Alphas')
