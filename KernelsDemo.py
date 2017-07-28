d622d8b0af61ad53f940166b7ef9fa7bfbf396e7
Partha Acharjee <partha.rlc@gmail.com>
2017-07-28 12:44:13 -0500

    Updated.

--- KernelsDemo.py
+++ KernelsDemo.py
@@ -1,54 +1,78 @@
 import numpy as np
 import matplotlib.pyplot as plt
+from mpl_toolkits.mplot3d import Axes3D
 from sklearn import svm
 
 plt.close('all')
 
+def plotclasses(x,y,plottitle='Plot classes on feature domain'):
+    ## Plot data
+    fig=plt.figure()
+    if np.shape(x)[1]==2:
+        plt.scatter(x[y,0],x[y,1],marker='.', color='r',alpha=0.5,label='Red Class')
+        plt.scatter(x[~y,0],x[~y,1],marker='.', color='g',alpha=0.5, label='Green Class')
+    elif np.shape(x)[1]==3:
+        ax = fig.add_subplot(111, projection='3d')
+        ax.scatter(x[y,0],x[y,1],zs=x[y,2],marker='.', color='r',alpha=0.5,label='Red Class')
+        ax.scatter(x[~y,0],x[~y,1],zs=x[~y,2],marker='.', color='g',alpha=0.5, label='Green Class')
+        ax.set_zlabel('X_3')
+        
+    
+    
+    plt.xlabel('X_1')
+    plt.ylabel('X_2')
+    plt.title(plottitle)
+    plt.legend()
+    plt.show()
+
+
+
 # Generate data
-N=500;
+N=2000;
 x=np.random.uniform(-1,1,(N,2))
 y=np.sqrt(x[:,0]**2+x[:,1]**2)<0.5
+plotclasses(x,y)
 
 
-xtest=np.random.uniform(-1,1,(N,2))
-ytest=np.sqrt(xtest[:,0]**2+xtest[:,1]**2)<0.5
+## SVM classification
 
-## Plot data
-plt.scatter(x[y,0],x[y,1],marker='.', color='r',alpha=0.5,label='Red Class')
-plt.scatter(x[~y,0],x[~y,1],marker='.', color='g',alpha=0.5, label='Green Class')
-
-plt.xlabel('X_1')
-plt.ylabel('X_2')
-plt.title('Class map in feature dimension')
-plt.legend()
-plt.show()
-
-# SVM classification
-
-clf = svm.SVC()
+clf = svm.LinearSVC()
 clf.fit(x, y)
-yest=clf.predict(xtest)
+yest=clf.predict(x)
+plotclasses(x,yest,'Linear SVM results')
 
+
+
+# Manually introduce a kernel
+X=np.zeros((N,3))
+X[:,0:2]=x
+X[:,2]=(x[:,0]**2+x[:,1]**2)**0.5
+
+clf2 = svm.LinearSVC()
+clf2.fit(X, y)
+Yest=clf2.predict(X)
+plotclasses(X,Yest,'Manual kernel results')
+
+# RBF introduce a kernel
+
+clf3 = svm.SVC()
+clf3.fit(x, y)
+yrbf=clf3.predict(x)
+plotclasses(x,yrbf,'RBF kernel results')
 
 # Plotting decision regions
-x_min, x_max = x[:, 0].min() - 0.1, x[:, 0].max() + 0.1
-y_min, y_max = x[:, 1].min() - 0.1, x[:, 1].max() + 0.1
-xx, yy = np.meshgrid(np.arange(x_min, x_max, 0.01),
-                     np.arange(y_min, y_max, 0.01))
+#plt.figure()
+#x_min, x_max = x[:, 0].min() - 0.1, x[:, 0].max() + 0.1
+#y_min, y_max = x[:, 1].min() - 0.1, x[:, 1].max() + 0.1
+#xx, yy = np.meshgrid(np.arange(x_min, x_max, 0.01),
+#                     np.arange(y_min, y_max, 0.01))
+#
+#Z = clf.predict(np.c_[xx.ravel(), yy.ravel()])
+#Z = Z.reshape(xx.shape)
+#
+#
+#
+#
+#plt.contourf(xx, yy, Z, alpha=0.4,label='SVM boundaries')
 
-Z = clf.predict(np.c_[xx.ravel(), yy.ravel()])
-Z = Z.reshape(xx.shape)
-
-
-
-plt.figure()
-plt.contourf(xx, yy, Z, alpha=0.4,label='SVM boundaries')
-plt.scatter(xtest[yest,0],xtest[yest,1],marker='.', color='r',alpha=0.5,label='Red Testset')
-plt.scatter(xtest[~yest,0],xtest[~yest,1],marker='.', color='g',alpha=0.5, label='Green Testset')
-
-plt.xlabel('X_1')
-plt.ylabel('X_2')
-plt.title('Class map in feature dimension')
-plt.legend()
-plt.show()
 
